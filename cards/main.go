4830d591f9f691e51d33ca2d4d87387c7193e130
katyasoup <campbell.katharine@gmail.com>
2018-02-22 13:06:56 -0600

    added saveToFile function

--- cards/main.go
+++ cards/main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5)
-	// // see deal function in deck.go, returns two values of type deck, so assign to two vars
-
-	// hand.print()
-	// remainingCards.print()
-
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
