ec0d85133aaf53bd51822df8b0a15b4bb9d2d664
katyasoup <campbell.katharine@gmail.com>
2018-02-22 11:02:26 -0600

    initial commit for cards project, with notes iin comments

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+
+	// only use := when declaring new var, not reassigning!
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
