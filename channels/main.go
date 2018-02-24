03b71c314d5ed6d29abae3a9797e89ce8eec0528
katyasoup <campbell.katharine@gmail.com>
2018-02-24 10:58:53 -0600

    channels lesson

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,26 @@ func main() {
 		"http://katyasoup.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println("Hmm, looks like", link, "might be down :(")
+		c <- "Channel says: Oof, might be down"
 		return
 	}
 
 	fmt.Println("All gravy,", link, "is up! :)")
+	c <- "Channel says: Yep, it's up"
 }
